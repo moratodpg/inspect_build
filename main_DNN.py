2948e966f18989a3a3e670762787e337c82096ee
moratodpg <moratosotelo@gmail.com>
2024-02-26 22:31:38 +0100

    updated main file

--- main_DNN.py
+++ main_DNN.py
@@ -1,25 +1,33 @@
-# import os
-# from datetime import datetime
-# import pickle
-# import argparse
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, random_split, Subset
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 
 from models.dnn import SimpleNet, Trainer, MLP_dropout
 from datasets.buildings_dataset import Buildings
+from active_learning.active_learn import ActiveLearning
+
 ## Input data
 dataset_th_file = "datasets/subset_build_6kB_dataset.pth"
 
-batch_size = 64
-train_ratio = 0.8
+num_train = 30
+num_test = 1500
+
+batch_size = 32
 learning_rate = 0.0001
-num_epochs = 50
-hidden_size = 200
+num_epochs = 200
+hidden_size = 100
+layers = 2
 num_classes = 2
+dropout_rate = 0.1
 # num_classes = 18
+
+num_active_iter = 1000
+num_forwards = 100
+
 
 # Assuming you have created a Buildings dataset instance named buildings_dataset
 buildings_dataset = torch.load(dataset_th_file)
@@ -28,46 +36,63 @@ buildings_dataset = torch.load(dataset_th_file)
 total_samples = len(buildings_dataset)
 print("Total number of samples in the dataset: ", total_samples)
 
-# Calculate the number of samples for training and testing
-num_train = int(train_ratio * total_samples)
-num_test = total_samples - num_train
+num_pool = total_samples - num_train - num_test
+
+# Generate random indices for the downscaled dataset
+# Ensure reproducibility with a fixed seed, if necessary
+# np.random.seed(42)  # Uncomment to make the selection reproducible
+# indices = np.random.choice(len(buildings_dataset), downscaled_size, replace=False)
 
 # Use random_split to split the dataset into training and testing sets
-train_dataset, test_dataset = random_split(buildings_dataset, [num_train, num_test])
-## In active learning: train_dataset will include new buildings, while test_dataset will have less buildings
+train_ds, test_ds, pool_ds = random_split(buildings_dataset, [num_train, num_test, num_pool])
 
-# Create DataLoader for training set
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-# Create DataLoader for testing set
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+print("Number of samples in the training set: ", len(train_ds))
+print("Number of samples in the testing set: ", len(test_ds))
+print("Number of samples in the pool set: ", len(pool_ds))
 
-# Instantiate the classifier
-input_size = len(train_dataset[0][0]) # It should be stored in logs
+### To reset the datasets
+# train_ds_0 = train_ds
+# test_ds_0 = test_ds
+# pool_ds_0 = pool_ds
 
-# net = SimpleNet(input_size, hidden_size, num_classes)
-net = MLP_dropout(input_size, hidden_size, 2, num_classes, 0.1)
+test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+f1_score_AL = []
+active_learn = ActiveLearning(num_active_points=1)
+
+for i in range(num_active_iter):
+    print("Active learning iteration: ", i)
+    # Create DataLoaders
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    pool_loader = DataLoader(pool_ds, batch_size=len(pool_ds), shuffle=False)
 
 
-# Define loss function and optimizer
-# criterion = FocalLoss()
-criterion = nn.CrossEntropyLoss()
-# criterion = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 5.499674613973851]))
-# criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.15385385567610865, 0.8461461443238913]))
-# criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.8461461443238913, 0.15385385567610865]))
-# criterion = nn.BCEWithLogitsLoss()
+    # Instantiate the classifier
+    input_size = len(train_ds[0][0]) # It should be stored in logs
+    input_size = 384 # Only considering aerial images
+    net = MLP_dropout(input_size, hidden_size, layers, num_classes, dropout_rate)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    trainer = Trainer(net, train_loader, test_loader, criterion, optimizer, num_epochs, patience=400)
 
-optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    trainer.train()
+    f1_score_AL.append(trainer.f1_score)
+    
+    ## Loop
+    idx_pool = pool_ds.indices
+    idx_train = train_ds.indices
 
-# Instantiate TrainerTester
-trainer = Trainer(net, train_loader, test_loader, criterion, optimizer, num_epochs)
-# trainer = MultiClassTrainer(net, train_loader, test_loader, criterion, optimizer, num_epochs, num_classes)
+    trainer.model.load_state_dict(trainer.best_model)
 
-# Train and test the model
-trainer.train()
+    ## Get active points
+    selected_idx_pool = active_learn.get_active_points(trainer.model, num_forwards, buildings_dataset, idx_pool)
+    
+    ## Updated indices based on selected samples
+    idx_pool_ = [idx for idx in idx_pool if idx not in selected_idx_pool]
+    idx_train_ = idx_train + selected_idx_pool
 
-torch.save(
-    {
-    'model_state_dict': trainer.model.state_dict(),
-    'optimizer_state_dict': trainer.optimizer.state_dict(),
-    }, 
-    'results/dropout.pth')
+    ## Updated subdatasets based on selected samples
+    train_ds = torch.utils.data.dataset.Subset(buildings_dataset, idx_train_) 
+    pool_ds = torch.utils.data.dataset.Subset(buildings_dataset, idx_pool_) 
+
+    print(len(pool_ds), len(train_ds))
